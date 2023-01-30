d7ad1d1d666c1174efb205b37d67b5e0225ac354
mauromatapro <drmauromata@hotmail.com>
2023-01-30 16:34:00 -0300

    Updated

---
+++ js/funtion.js
@@ -0,0 +1,26 @@
+ 
+//Menú desplegable
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+//Botón Ir Arriba
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0; // Para Safari
+    document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE and Opera
+}
+ 
